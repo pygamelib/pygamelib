e48f5138f1eb7932c29dd2708db98d7aa2d39c75
Arnaud Dupuis <arnaud.l.dupuis@gmail.com>
2019-03-15 09:49:07 -0700

    Add Utils class for utility functions

---
+++ gamelib/Utils.py
@@ -0,0 +1,13 @@
+from colorama import Fore, Back, Style
+
+def warn(message):
+    print(Fore.BLACK + Back.YELLOW + 'WARNING' + Style.RESET_ALL + ': '+message)
+
+def fatal(message):
+    print(Fore.WHITE + Back.RED + Style.BRIGHT + 'FATAL' + Style.RESET_ALL + ': '+message)
+
+def info(message):
+    print(Fore.WHITE + Back.BLUE + 'INFO' + Style.RESET_ALL + ': '+message)
+
+def debug(message):
+    print(Fore.WHITE + Back.GREEN + Style.BRIGHT + 'DEBUG' + Style.RESET_ALL + ': '+message)
