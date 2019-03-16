d9de9e62001ed4b5c45af5deb2de96d4381c6b1c
Arnaud Dupuis <arnaud.l.dupuis@gmail.com>
2019-03-16 16:36:30 -0700

    :cat: change base_game to test stuff

--- base_game.py
+++ base_game.py
@@ -1,8 +1,19 @@
 from gamelib.Board import Board
+from gamelib.BoardItem import BoardItem
 from gamelib.Utils import warn, fatal, info,debug
 
-board = Board(name='Level_1',size=[80,80])
+from gamelib.Structures import Wall,Treasure
+
+board = Board(name='Level_1',size=[20,10])
 print(board)
 
 debug('displaying empty board')
+board.display()
+
+
+w = Wall()
+t = Treasure()
+
+board.place_item(w,3,3)
+board.place_item(t,3,2)
 board.display()
