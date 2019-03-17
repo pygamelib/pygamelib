4e134e6c47cc34d14b8c4c0088fee56bc0fb3b84
Arnaud Dupuis <arnaud.l.dupuis@gmail.com>
2019-03-16 17:06:39 -0700

    :zap: base_game uses the game class

--- base_game.py
+++ base_game.py
@@ -1,8 +1,11 @@
 from gamelib.Board import Board
 from gamelib.BoardItem import BoardItem
 from gamelib.Utils import warn, fatal, info,debug
+from gamelib.Game import Game
 
 from gamelib.Structures import Wall,Treasure
+
+game = Game(name='HAC Game')
 
 board = Board(name='Level_1',size=[20,10])
 print(board)
@@ -17,3 +20,12 @@ t = Treasure()
 board.place_item(w,3,3)
 board.place_item(t,3,2)
 board.display()
+
+game.add_menu_entry('w','Go up')
+game.add_menu_entry('s','Go down')
+game.add_menu_entry('a','Go left')
+game.add_menu_entry('d','Go right')
+
+game.clear_screen()
+board.display()
+game.print_menu()
