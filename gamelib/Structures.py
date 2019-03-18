932cde212d19999677ea835396b30d6a845a4830
Arnaud Dupuis <arnaud.l.dupuis@gmail.com>
2019-03-18 14:57:44 -0700

    :zap: added a GenericStructure object.

--- gamelib/Structures.py
+++ gamelib/Structures.py
@@ -4,6 +4,8 @@ class Wall(Immovable):
     def __init__(self,**kwargs):
         if 'model' not in kwargs.keys():
             kwargs['model'] = '#'
+        if 'name' not in kwargs.keys():
+            kwargs['name'] = 'wall'
         Immovable.__init__(self,**kwargs)
     
     def pickable(self):
@@ -11,6 +13,30 @@ class Wall(Immovable):
     
     def overlappable(self):
         return False
+
+class GenericStructure(Immovable):
+    def __init__(self,**kwargs):
+        if 'model' not in kwargs.keys():
+            kwargs['model'] = '#'
+        if 'name' not in kwargs.keys():
+            kwargs['name'] = 'wall'
+        Immovable.__init__(self,**kwargs)
+        self.__is_pickable = False
+        self.__is_overlappable = False
+    
+    def pickable(self):
+        return self.__is_pickable
+
+    def set_pickable(self,val):
+        if type(val) is bool:
+            self.__is_pickable = val
+
+    def overlappable(self):
+        return self.__is_overlappable
+
+    def set_overlappable(self,val):
+        if type(val) is bool:
+            self.__is_overlappable = val
 
 
 class Treasure(Immovable):
