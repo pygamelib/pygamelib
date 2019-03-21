5b1307f62bbe313685e85101ac28e35323609eb3
Arnaud Dupuis <arnaud.l.dupuis@gmail.com>
2019-03-21 01:46:46 -0700

    :hammer: Refactor & improvements Refactored the menu system. Added a list of NPC to the notion opf level (not only a board anymore). Added NPC management to Game(). Add & move ATM.

--- gamelib/Game.py
+++ gamelib/Game.py
@@ -1,9 +1,14 @@
 import os
-from gamelib.HacExceptions import HacInvalidTypeException, HacInvalidLevelException
+from gamelib.HacExceptions import HacInvalidTypeException, HacInvalidLevelException, HacException
 from gamelib.Board import Board
+from gamelib.BoardItem import BoardItemVoid
+from gamelib.Characters import NPC
+from gamelib.Actuators.SimpleActuators import RandomActuator
+import gamelib.Constants as C
+import random
 
 class Game():
-    def __init__(self,name='Game',boards = {}, menu = [], current_level = None):
+    def __init__(self,name='Game',boards = {}, menu = {}, current_level = None):
         self.name = name
         self._boards = boards
         self._menu = menu
@@ -11,11 +16,16 @@ class Game():
         self.player = None
 
     
-    def add_menu_entry(self,shortcut,message):
-        self._menu.append({'shortcut' : shortcut,'message':message})
+    def add_menu_entry(self,category,shortcut,message):
+        if category in self._menu.keys():
+            self._menu[category].append({'shortcut' : shortcut,'message':message})
+        else:
+            self._menu[category] = [{'shortcut' : shortcut,'message':message}]
     
-    def print_menu(self):
-        for k in self._menu:
+    def print_menu(self,category):
+        if category not in self._menu:
+            raise HacException('invalid_menu_category',f"The '{category}' category is not registered in the menu. Did you add any menu entry in that category with Game.add_menu_entry('{category}','some shortcut','some message') ? If yes, then you should check for typos." )
+        for k in self._menu[category]:
             if k['shortcut'] == None:
                 print(k['message'])
             else:
@@ -36,11 +46,11 @@ class Game():
         """
         if type(level_number) is int:
             if isinstance(board, Board):
-                self._boards[level_number] = board
+                self._boards[level_number] = {'board':board,'npcs':[]}
             else:
                 raise HacInvalidTypeException("The board paramater must be a gamelib.Board.Board() object.")
         else:
-            raise HacInvalidTypeException("The board number must be an int.")
+            raise HacInvalidTypeException("The level number must be an int.")
     
     def current_board(self):
         """
@@ -50,7 +60,7 @@ class Game():
         If current_level is set to a value with no corresponding board a HacException excpetion is raised with an invalid_level error.
         """
         if  self.current_level in self._boards.keys():
-            return self._boards[self.current_level]
+            return self._boards[self.current_level]['board']
         else:
             raise HacInvalidLevelException("The current level does not correspond to any board.")
     
@@ -67,11 +77,65 @@ class Game():
         if type(level_number) is int:
             if  level_number in self._boards.keys():
                 if self.player.pos[0] != None or self.player.pos[1] != None:
-                    self._boards[self.current_level].clear_cell(self.player.pos[0],self.player.pos[1]) 
+                    self._boards[self.current_level]['board'].clear_cell(self.player.pos[0],self.player.pos[1]) 
                 self.current_level = level_number
-                b = self._boards[self.current_level]
+                b = self._boards[self.current_level]['board']
                 b.place_item(self.player,b.player_starting_position[0],b.player_starting_position[1])
             else:
                 raise HacInvalidLevelException(f"Impossible to change level to an unassociated level (level number {level_number} is not associated with any board).\nHave you called:\ngame.add_board({level_number},Board()) ?")
         else:
             raise HacInvalidTypeException('level_number needs to be an int in change_level(level_number).')
+        
+    def add_npc(self,level_number,npc,x=None,y=None):
+        """
+        Add a NPC to the game. It will be placed on the board corresponding to the level_number.
+        If x and y are not None, the NPC is placed at these coordinates. Else, it's randomly placed in an empy cell.
+        Ex: game.add_board(1,myboard)
+
+         - level_number : the level number of the board. Must be an int.
+         - npc : the NPC to place. Must be a gamelib.Characters.NPC (or a subclass of it)
+         - x and y: the coordinates to place the NPC. Must be an int.
+
+         If either of these parameters are not of the correct type, a HacInvalidTypeException exception is raised.
+
+         IMPORTANT: If the NPC does not have an actuator, this method is going to affect a gamelib.Actuators.SimpleActuators.RandomActuator() to npc.actuator.
+                    And if npc.step == None, this method sets it to 1
+        """
+        if type(level_number) is int:
+            if isinstance(npc, NPC):
+                if x == None or y == None:
+                    retry = 0
+                    while True :
+                        if x == None:
+                            x = random.randint(0,self._boards[level_number]['board'].size[1]-1)
+                        if y == None:
+                            y = random.randint(0,self._boards[level_number]['board'].size[0]-1)
+                        # print(f"Game.add_npc() finding a position for NPC {npc.name} trying ({x},{y})")
+                        if isinstance(self._boards[level_number]['board'].item(x,y), BoardItemVoid):
+                            break
+                        else:
+                            x = None
+                            y = None
+                            retry += 1
+                if type(x) is int:
+                    if type(y) is int:
+                        if npc.actuator == None:
+                            npc.actuator = RandomActuator(moveset=[C.UP,C.DOWN,C.LEFT,C.RIGHT])
+                        if npc.step == None:
+                            npc.step = 1
+                        self._boards[level_number]['board'].place_item(npc,x,y)
+                        self._boards[level_number]['npcs'].append(npc)
+                    else:
+                        raise HacInvalidTypeException("y must be an int.")
+                else:
+                    raise HacInvalidTypeException("x must be an int.")
+            else:
+                raise HacInvalidTypeException("The npc paramater must be a gamelib.Characters.NPC() object.")
+        else:
+            raise HacInvalidTypeException("The level number must be an int.")
+        
+    def move_npcs(self,level_number):
+        # TODO: Documentation
+        # TODO: Check data type and availability
+        for npc in self._boards[level_number]['npcs']:
+            self._boards[level_number]['board'].move(npc, npc.actuator.next_move(), npc.step)
