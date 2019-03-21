bf6ca46ddedefcd69adbcd7f584a59d660449bb3
Arnaud Dupuis <arnaud.l.dupuis@gmail.com>
2019-03-21 01:41:54 -0700

    :zap: NPCs are now using actuators.

--- gamelib/Characters.py
+++ gamelib/Characters.py
@@ -19,9 +19,20 @@ class Player(Movable):
 class NPC(Movable):
     """
     A class that represent a non playable character controlled by the computer.
+    For the NPC to be successfully managed by the Game, you need to set an actuator.
+    Ex: mynpc.actuator = RandomActuator()
     """
     def __init__(self,**kwargs):
         Movable.__init__(self,**kwargs)
+        if 'actuator' not in kwargs.keys():
+            self.actuator= None
+        else:
+            self.actuator = kwargs['actuator']
+        
+        if 'step' not in kwargs.keys():
+            self.step = None
+        else:
+            self.step = kwargs['step']
     
     def pickable(self):
         return False
