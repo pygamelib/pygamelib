da627ac85cc2c7d1ab9ffdf01be0ddb464bddabe
Arnaud Dupuis <arnaud.l.dupuis@gmail.com>
2019-03-30 01:57:37 -0700

    :sparkles: Started to work on the editor.

---
+++ editor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+import gamelib.Utils as Utils
+import gamelib.Constants as Constants
+from gamelib.Game import Game
+
+print( Utils.cyan_bright("HAC-GAME-LIB - EDITOR v"+Constants.HAC_GAME_LIB_VERSION) )
