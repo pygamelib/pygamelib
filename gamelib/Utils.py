5f69cd850a6c1aba6cf2c00240045254d1aa16ca
Arnaud Dupuis <arnaud.l.dupuis@gmail.com>
2019-03-31 10:16:29 -0700

    :books: Finish documentation of Utils

--- gamelib/Utils.py
+++ gamelib/Utils.py
@@ -79,15 +79,59 @@ def get_key():
 
 ## the warn() function print a message prefixed by a yellow WARNING.
 def warn(message):
+    """Print a warning message.
+
+    The warning is a regular message prefixed by WARNING in black on a yellow background.
+
+    :param message: The message to print.
+    :type message: str
+    
+    Example::
+    
+        Utils.warn("This is a warning.")
+    """
     print(Fore.BLACK + Back.YELLOW + 'WARNING' + Style.RESET_ALL + ': '+message)
 
 def fatal(message):
+    """Print a fatal message.
+
+    The fatal message is a regular message prefixed by FATAL in white on a red background.
+
+    :param message: The message to print.
+    :type message: str
+    
+    Example::
+    
+        Utils.fatal("|x_x|")
+    """
     print(Fore.WHITE + Back.RED + Style.BRIGHT + 'FATAL' + Style.RESET_ALL + ': '+message)
 
 def info(message):
+    """Print an informative message.
+
+    The info is a regular message prefixed by INFO in white on a blue background.
+
+    :param message: The message to print.
+    :type message: str
+    
+    Example::
+    
+        Utils.info("This is a very informative message.")
+    """
     print(Fore.WHITE + Back.BLUE + 'INFO' + Style.RESET_ALL + ': '+message)
 
 def debug(message):
+    """Print a debug message.
+
+    The debug messgae is a regular message prefixed by INFO in blue on a green background.
+
+    :param message: The message to print.
+    :type message: str
+    
+    Example::
+    
+        Utils.debug("This is probably going to success, eventually...")
+    """
     print(Fore.BLUE + Back.GREEN + Style.BRIGHT + 'DEBUG' + Style.RESET_ALL + ': '+message)
 
 def print_white_on_red(message):
@@ -95,75 +139,157 @@ def print_white_on_red(message):
 
 # Colored bright functions
 def green_bright(message):
+    """
+    Return a string formatted to be bright green
+
+    :param message: The message to format.
+    :type message: str
+    :return: The formatted string
+    :rtype: str
+
+    Example::
+
+        print( Utils.green_bright("This is a formatted message") )
+
+    """
     return Fore.GREEN+Style.BRIGHT+message+Style.RESET_ALL
 
 def blue_bright(message):
+    """
+    This method works exactly the way green_bright() work with different color.
+    """
     return Fore.BLUE+Style.BRIGHT+message+Style.RESET_ALL
 
 def red_bright(message):
+    """
+    This method works exactly the way green_bright() work with different color.
+    """
     return Fore.RED+Style.BRIGHT+message+Style.RESET_ALL
 
 def yellow_bright(message):
+    """
+    This method works exactly the way green_bright() work with different color.
+    """
     return Fore.YELLOW+Style.BRIGHT+message+Style.RESET_ALL
 
 def magenta_bright(message):
+    """
+    This method works exactly the way green_bright() work with different color.
+    """
     return Fore.MAGENTA+Style.BRIGHT+message+Style.RESET_ALL
 
 def cyan_bright(message):
+    """
+    This method works exactly the way green_bright() work with different color.
+    """
     return Fore.CYAN+Style.BRIGHT+message+Style.RESET_ALL
 
 def white_bright(message):
+    """
+    This method works exactly the way green_bright() work with different color.
+    """
     return Fore.WHITE+Style.BRIGHT+message+Style.RESET_ALL
 
 def black_bright(message):
+    """
+    This method works exactly the way green_bright() work with different color.
+    """
     return Fore.BLACK+Style.BRIGHT+message+Style.RESET_ALL
 
 # Colored normal functions
 def green(message):
+    """
+    This method works exactly the way green_bright() work with different color.
+    """
     return Fore.GREEN+message+Style.RESET_ALL
 
 def blue(message):
+    """
+    This method works exactly the way green_bright() work with different color.
+    """
     return Fore.BLUE+message+Style.RESET_ALL
 
 def red(message):
+    """
+    This method works exactly the way green_bright() work with different color.
+    """
     return Fore.RED+message+Style.RESET_ALL
 
 def yellow(message):
+    """
+    This method works exactly the way green_bright() work with different color.
+    """
     return Fore.YELLOW+message+Style.RESET_ALL
 
 def magenta(message):
+    """
+    This method works exactly the way green_bright() work with different color.
+    """
     return Fore.MAGENTA+message+Style.RESET_ALL
 
 def cyan(message):
+    """
+    This method works exactly the way green_bright() work with different color.
+    """
     return Fore.CYAN+message+Style.RESET_ALL
 
 def white(message):
+    """
+    This method works exactly the way green_bright() work with different color.
+    """
     return Fore.WHITE+message+Style.RESET_ALL
 
 def black(message):
+    """
+    This method works exactly the way green_bright() work with different color.
+    """
     return Fore.BLACK+message+Style.RESET_ALL
 
 # Colored dim function
 def green_dim(message):
+    """
+    This method works exactly the way green_bright() work with different color.
+    """
     return Fore.GREEN+Style.DIM+message+Style.RESET_ALL
 
 def blue_dim(message):
+    """
+    This method works exactly the way green_bright() work with different color.
+    """
     return Fore.BLUE+Style.DIM+message+Style.RESET_ALL
 
 def red_dim(message):
+    """
+    This method works exactly the way green_bright() work with different color.
+    """
     return Fore.RED+Style.DIM+message+Style.RESET_ALL
 
 def yellow_dim(message):
+    """
+    This method works exactly the way green_bright() work with different color.
+    """
     return Fore.YELLOW+Style.DIM+message+Style.RESET_ALL
 
 def magenta_dim(message):
+    """
+    This method works exactly the way green_bright() work with different color.
+    """
     return Fore.MAGENTA+Style.DIM+message+Style.RESET_ALL
 
 def cyan_dim(message):
+    """
+    This method works exactly the way green_bright() work with different color.
+    """
     return Fore.CYAN+Style.DIM+message+Style.RESET_ALL
 
 def white_dim(message):
+    """
+    This method works exactly the way green_bright() work with different color.
+    """
     return Fore.WHITE+Style.DIM+message+Style.RESET_ALL
 
 def black_dim(message):
+    """
+    This method works exactly the way green_bright() work with different color.
+    """
     return Fore.BLACK+Style.DIM+message+Style.RESET_ALL
