4fcca5db71fcababae2276e9deeae2bf4d8d2da5
Arnaud Dupuis <arnaud.l.dupuis@gmail.com>
2019-04-08 09:22:07 -0700

    :zap: test_board_loading.py now takes a second argument : number of iiterations.

--- test_board_loading.py
+++ test_board_loading.py
@@ -9,24 +9,27 @@ board_to_load = 'hac-maps/test-board.json'
 if len(sys.argv) > 1:
     board_to_load = sys.argv[1]
 
+max_iter = 10
+if len(sys.argv) > 2:
+    max_iter = int(sys.argv[2])
+
+
 g = Game()
 
 b = g.load_board(board_to_load,1)
 
 print(b)
 
-g.player = Player(model=Sprites.HORSE)
+g.player = Player(model=Sprites.FLYING_SAUCER)
 g.change_level(1)
 
 # print('Printing the complete list of sprites')
 # print([getattr(Sprites,item) for item in dir(Sprites) if not item.startswith("__")])
 idx=0
-while idx< 10: 
+while idx< max_iter: 
     g.clear_screen()
     g.actuate_npcs(1)
     g.display_board()
 
     time.sleep(0.1)
     idx += 1
-if str(input(f'Save this map as {board_to_load}? (y/n): ')) == 'y':
-    g.save_board(1,board_to_load)
