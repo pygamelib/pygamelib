99743bd675c301bb2e1fca8007b626a895c75140
Arnaud Dupuis <arnaud.l.dupuis@gmail.com>
2019-04-22 08:47:49 -0700

    :zap: Add some project URLs and Python restrictions to setup.py

--- setup.py
+++ setup.py
@@ -12,11 +12,19 @@ setuptools.setup(
     description="A small game development framework for teaching programming to young kids.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/arnauddupuis/hac-game-lib",
+    url="https://astro.hyrul.es",
     packages=setuptools.find_packages(),
+    keywords='game development beginner',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
+    project_urls={
+        'Documentation': 'https://hac-game-lib.readthedocs.io/en/latest/index.html',
+        'Source': 'https://github.com/arnauddupuis/hac-game-lib',
+        'Tracker': 'https://github.com/arnauddupuis/hac-game-lib/issues',
+        'Release Notes' : 'https://astro.hyrul.es/release-notes/2019.4a1/'
+    },
+    python_requires='>=3',
 )
