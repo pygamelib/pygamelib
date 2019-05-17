ccf36b190836c5373e1cbc2d45b65b82536888d9
Arnaud Dupuis <arnaud.l.dupuis@gmail.com>
2019-05-17 08:34:35 -0700

    :open_book: Adding more content to tutorials.

---
+++ examples/Readme.md
@@ -0,0 +1,21 @@
+# Examples
+
+## Introduction
+
+In this folder you will find 2 different kinds of examples: 
+ 1. Code examples (filename starts with a number like '01_board_and_wall.py')
+ 2. Implementation example (mostly tutorials like 'tutorial_01.py')
+
+## Code examples
+
+These are small code examples on how to use a couple of specific objects.  
+They are focusing on using the object by itself. Not necessarily in a "good" way. 
+
+For example, the 06_inventory.py script gives example on using the Inventory class. But that example is just showing how to manipulate the inventory by adding, retrieving or removing objects from the inventory. It is not showing a live example of a working inventory system in a game (have a look at hgl-base_game.py for that).
+
+## Implementation examples
+
+On the contrary, tutorial_[0-9+].py files are implementation examples of how the features of the hac-game-lib could (and probably should) be used in real game developments.
+
+They are **always** linked to an article on [http://astro.hyrule.com](http://astro.hyrule.com) and most of the time also to a video on [Youtube](https://www.youtube.com/channel/UCT_SxIlKaD6MM7JlQKelpgw?&ab_channel=HyruleAstronomyClub).  
+The link to the article is always in the comments of the Python file.
---
+++ examples/tutorial_01.py
@@ -0,0 +1,38 @@
+import examples_includes
+from gamelib.Game import Game
+from gamelib.Board import Board
+import gamelib.Utils as Utils
+import gamelib.Sprites as Sprites
+from gamelib.Characters import Player
+import time
+
+#################################################################################
+##                                                                             ##
+## IMPORTANT NOTE:                                                             ##
+##                                                                             ##
+## The companion article for this tutorial code is at:                         ##
+## https://astro.hyrul.es/guides/hac-game-lib/tutorial-01-the-game-object.html ##
+##                                                                             ##
+#################################################################################
+
+mygame = Game(name='Demo game')
+board1 = Board(name='Level 1', ui_borders=Sprites.WALL, ui_board_void_cell=Utils.BLACK_SQUARE, player_starting_position=[0,0])
+board2 = Board(name='Level 2', ui_borders=Utils.RED_SQUARE, ui_board_void_cell=Utils.BLACK_SQUARE, player_starting_position=[4,4])
+
+mygame.player = Player(name='DaPlay3r', model=Sprites.UNICORN_FACE)
+
+mygame.add_board(1,board1)
+mygame.add_board(2,board2)
+
+count = 0 
+lvl = 1
+
+while count < 10:
+    mygame.clear_screen()
+    mygame.change_level(lvl)
+    mygame.current_board().display()
+    lvl +=1 
+    if lvl > 2:
+        lvl = 1
+    time.sleep(2)
+    count += 1
