a4cf10ebf6ced6a2c62dd7cc6a8565664a14c23a
Arnaud Dupuis <arnaud.l.dupuis@gmail.com>
2019-08-23 16:04:51 -0700

    :pencil: Add some documentation to Actuator.

--- gamelib/Actuators/Actuator.py
+++ gamelib/Actuators/Actuator.py
@@ -6,12 +6,32 @@ class Actuator():
         self.state = RUNNING
     
     def start(self):
+        """Set the actuator state to RUNNING.
+
+        If the actuator state is not RUNNING, actuators' next_move() function (and all derivatives) should not return anything.
+
+        Example::
+        
+            mygame.start()
+        """
         self.state = RUNNING
     
     def pause(self):
+        """Set the actuator state to PAUSED.
+
+        Example::
+        
+            mygame.pause()
+        """
         self.state = PAUSED
     
     def stop(self):
+        """Set the actuator state to STOPPED.
+        
+        Example::
+        
+            mygame.stop()
+        """
         self.state = STOPPED
 
     def next_move(self):
