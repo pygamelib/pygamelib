3696ceb342d7e154a6d74bce55aaa3782256747d
Chase Miller <chasemillerwd@gmail.com>
2019-10-04 16:21:39 -0400

    Add test_clear_cell

--- tests/test_board.py
+++ tests/test_board.py
@@ -1,5 +1,5 @@
 from gamelib.Board import Board
-from gamelib.BoardItem import BoardItem
+from gamelib.BoardItem import BoardItem, BoardItemVoid
 from gamelib.HacExceptions import HacOutOfBoardBoundException
 import unittest
 
@@ -19,6 +19,15 @@ class TestBoard(unittest.TestCase):
         with self.assertRaises(HacOutOfBoardBoundException) as excinfo:
             self.board.item(15,15)
         self.assertTrue("out of the board boundaries" in str(excinfo.exception))
+
+    def test_clear_cell(self):
+        self.board = Board(name='test_board',size=[10,10],player_starting_postiion=[5,5])
+        self.placed_item = BoardItem()
+        self.board.place_item(item=self.placed_item,row=1,column=1)
+        self.assertIsInstance(self.board.item(1,1), BoardItem)
+
+        self.board.clear_cell(1,1)
+        self.assertIsInstance(self.board.item(1,1), BoardItemVoid)
 
 
 if __name__ == '__main__':
