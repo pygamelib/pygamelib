46e69044b226848f7543352da6e053ff1f9f5b9e
Kalil de Lima <kalil@rootstrap.com>
2019-10-05 10:45:14 -0300

    add patrol actuator and fix late binding issues

--- gamelib/Actuators/SimpleActuators.py
+++ gamelib/Actuators/SimpleActuators.py
@@ -12,15 +12,15 @@ class RandomActuator(Actuator):
 
     ..todo:: finish the doc.
     """
-    def __init__(self,moveset=[]):
-        Actuator.__init__(self)
+    def __init__(self,moveset=None):
+        if moveset is None:
+            moveset = []
+        super.__init__(self)
         self.moveset = moveset
     
     def next_move(self):
         if self.state == RUNNING:
             return random.choice(self.moveset)
-        else:
-            return None
 
 class PathActuator(Actuator):
     """ 
@@ -30,7 +30,9 @@ class PathActuator(Actuator):
     :param path: A list of paths.
     :type path: list
     """
-    def __init__(self,path=[]):
+    def __init__(self,path=None):
+        if path is None:
+            path = []
         Actuator.__init__(self)
         self.path = path
         self.index = 0
@@ -42,10 +44,28 @@ class PathActuator(Actuator):
             if self.index == len(self.path):
                 self.index = 0
             return move
-        else:
-            return None
             
     def set_path(self,path):
         self.path = path
         self.index = 0
-        
+
+class PatrolActuator(PathActuator):
+    """
+    The patrol actuator is a subclass of :class:`~gamelib.Actuators.PathActuator`.  The move inside the function next_move
+    depends on path and index and the mode.
+    Once it reaches the end of the move list it will start cycling back to the beggining of the list.
+    Once it reaches the beggining it will start moving forwards
+    If the state is not running it returns None otherwise it increments the index & then, further compares the index 
+    with length of the path. If they both are same then, index is set to value zero and the move is returned back.
+    :param path: A list of paths.
+    :type path: list
+    """
+
+    def next_move(self):
+        if self.state == RUNNING:
+            move = self.path[self.index]
+            self.index += 1
+            if self.index == len(self.path):
+                self.index = 0
+                self.path = self.path.reverse()
+            return move
