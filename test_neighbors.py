7e71ebe611b00403975df6d41672629e89644d92
Guillermo Eijo <guillermo.eijo@rootstrap.com>
2019-10-09 16:28:00 -0300

    Fix flake 8 errors on test_neighbors.py

--- test_neighbors.py
+++ test_neighbors.py
@@ -6,31 +6,31 @@ import gamelib.Constants as Constants
 
 g = Game()
 
-b = g.load_board('hac-maps/kneighbors.json',1)
+b = g.load_board('hac-maps/kneighbors.json', 1)
 
 
-g.player = Player(model=Sprites.FLYING_SAUCER,name='player')
+g.player = Player(model=Sprites.FLYING_SAUCER, name='player')
 g.change_level(1)
 
 key = None
 
 while True:
     if key == 'w':
-        g.move_player(Constants.UP,1)
+        g.move_player(Constants.UP, 1)
     elif key == 's':
-        g.move_player(Constants.DOWN,1)
+        g.move_player(Constants.DOWN, 1)
     elif key == 'a':
-        g.move_player(Constants.LEFT,1)
+        g.move_player(Constants.LEFT, 1)
     elif key == 'd':
-        g.move_player(Constants.RIGHT,1)
+        g.move_player(Constants.RIGHT, 1)
     elif key == 'q':
         break
     g.clear_screen()
     g.display_board()
     for i in g.neighbors(1):
         print(f'Player: {i.name} ({i.pos[0]},{i.pos[1]})')
-    
-    for i in g.neighbors(1, g.current_board().item(7,7) ):
+
+    for i in g.neighbors(1, g.current_board().item(7, 7)):
         print(f'NPC: {i.name} ({i.pos[0]},{i.pos[1]})')
 
     key = Utils.get_key()
