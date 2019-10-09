15686e6d032aaf067a8dd1d19edbe4e1843b420f
Guillermo Eijo <guillermo.eijo@rootstrap.com>
2019-10-09 17:46:07 -0300

    Fix flake8 errors in setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,8 @@ setuptools.setup(
     version=Constants.HAC_GAME_LIB_VERSION,
     author="Arnaud Dupuis",
     author_email="hyrule.astronomy.club@gmail.com",
-    description="A small game development framework for teaching programming to young kids.",
+    description="A small game development framework for teaching \
+        programming to young kids.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=[
@@ -30,19 +31,19 @@ setuptools.setup(
     ],
     url="https://astro.hyrul.es",
     packages=setuptools.find_packages(),
-    scripts=['hgl-base_game.py','hgl-editor.py','hgl-board-tester.py'],
-    keywords=['game','development','beginner'],
+    scripts=['hgl-base_game.py', 'hgl-editor.py', 'hgl-board-tester.py'],
+    keywords=['game', 'development', 'beginner'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     project_urls={
-        'Documentation': 'https://hac-game-lib.readthedocs.io/en/latest/index.html',
+        'Documentation': 'https://hac-game-lib.readthedocs.io/en/latest/index.html',  # noqa: E501
         'Guides & Tutorials': 'https://astro.hyrul.es/guides/hac-game-lib/',
         'Source': 'https://github.com/arnauddupuis/hac-game-lib',
         'Tracker': 'https://github.com/arnauddupuis/hac-game-lib/issues',
-        'Release Notes' : 'https://astro.hyrul.es/release-notes/2019.4a1/'
+        'Release Notes': 'https://astro.hyrul.es/release-notes/2019.4a1/'
     },
     python_requires='>=3',
 )
