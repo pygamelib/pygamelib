f53ca072dfdcd975f37f1aa29f3bbb9174d57e12
f-osorio <f.osorio@zbw.eu>
2019-10-10 12:53:34 +0200

    [#39] Flake8 update for 03_player_move.py

--- examples/03_move_player.py
+++ examples/03_move_player.py
@@ -10,40 +10,44 @@ import gamelib.Utils as Utils
 import gamelib.Constants as Constants
 
 # First let's create a Board that uses squares as delimiters
-myboard = Board(
-                name='A demo board',
-                size=[40,20],
-                ui_border_left=Utils.WHITE_SQUARE, # Borders are going to be white squares
+# Borders are going to be white squares
+# Cells with nothing inside are going to be black squares
+myboard = Board(name='A demo board',
+                size=[40, 20],
+                ui_border_left=Utils.WHITE_SQUARE,
                 ui_border_right=Utils.WHITE_SQUARE,
                 ui_border_top=Utils.WHITE_SQUARE,
                 ui_border_bottom=Utils.WHITE_SQUARE,
-                ui_board_void_cell=Utils.BLACK_SQUARE, # Cells with nothing inside are going to be black squares
-                player_starting_position=[10,20]
+                ui_board_void_cell=Utils.BLACK_SQUARE,
+                player_starting_position=[10, 20]
                 )
 
-# Then create a player 
-playerone = Player( model=Utils.yellow_bright('××') )
+# Then create a player
+playerone = Player(model=Utils.yellow_bright('××'))
 
 # Place it on the board
-myboard.place_item(playerone, 10,10)
+myboard.place_item(playerone, 10, 10)
 
 # Now let's display our board
 myboard.display()
 
-# And make a short loop that moves the player and display the board 
-# WARNING: in real life we would use the Game object to manage the game and the screen
+# And make a short loop that moves the player and display the board
+# WARNING: in real life we would use the Game object to manage the game
+# and the screen
 
-for k in range(1,10,1):
+
+for k in range(1, 10, 1):
     # Clear screen
     Utils.clear_screen()
     # Print a debug message
-    Utils.debug(f'Round {k} player position is ({playerone.pos[0]},{playerone.pos[1]}) -- BEFORE moving')
+    Utils.debug(f'Round {k} player position is ({playerone.pos[0]},'
+                f'{playerone.pos[1]}) -- BEFORE moving')
     # Ask myboard to move playerone to the right by one cell
-    myboard.move(playerone, Constants.RIGHT, 1 )
+    myboard.move(playerone, Constants.RIGHT, 1)
     # print another debug message
-    Utils.debug(f'Round {k} player position is now ({playerone.pos[0]},{playerone.pos[1]}) -- AFTER moving')
+    Utils.debug(f'Round {k} player position is now ({playerone.pos[0]},'
+                f'{playerone.pos[1]}) -- AFTER moving')
     # and display the board
     myboard.display()
     # Wait a second to let you admire the work!
     time.sleep(1)
-
