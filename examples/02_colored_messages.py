d42dc731df2869dfc5ff97239f76af08ef392eb0
f-osorio <f.osorio@zbw.eu>
2019-10-10 10:37:52 +0200

    [#39] Flake8 update
    This is maybe a bigger change then is wanted. I tried to to keep the
    style of concatenation the same that is using `+`, but I couldn't find a
    good way to do it that flake8 liked. Breaking on the `+` created a
    message about a "line break before/after binary operator" and putting
    the break else where was real ugly.
    
    I also noticed that a number of files make use of f-strings and opted
    for that for one of the examples so that it's consistent with the other
    files and, I think, easier to read.

--- examples/02_colored_messages.py
+++ examples/02_colored_messages.py
@@ -6,12 +6,16 @@ import gamelib.Utils as Utils
 # Then get creative!
 
 # you directly print colored messages:
-print("France flag is "+Utils.blue_bright('blue ')+Utils.white_bright('white ')+Utils.red_bright('red')+"!")
+print(Utils.magenta_bright('This is a rather flashy magenta...'))
 
 # Each color function has 3 variations : regular, dim and bright
-print( Utils.yellow_dim('This is dim.')+Utils.yellow(' This is regular.')+Utils.yellow_bright(' This is bright.') )
+print(Utils.yellow_dim('This is dim.'))
+print(Utils.yellow('This is regular.'))
+print(Utils.yellow_bright('This is bright.'))
 
-# Now, the color functions are just that: functions. So you can store the results in a variable and use them:
-flashymagy = Utils.magenta_bright('This is a rather flashy magenta...')
-
-print('Now that is a colored message in a variable: '+flashymagy)
+# Now, the color functions are just that: functions. So you can store the
+# results in a variable and use them:
+blue = Utils.blue_bright('blue')
+white = Utils.white_bright('white')
+red = Utils.red_bright('red')
+print(f"France's flag is {blue} {white} {red}!")
