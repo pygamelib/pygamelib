327f6d28f4d374fdefc4b9daa244a3a126ea0b0a
Arnaud Dupuis <arnaud.l.dupuis@gmail.com>
2019-10-18 00:28:06 -0700

    :rotating_light: black + flake8 BoardItems

--- gamelib/BoardItem.py
+++ gamelib/BoardItem.py
@@ -1,42 +1,45 @@
-from gamelib.HacExceptions import HacException, HacInvalidTypeException
-
-class BoardItem():
+class BoardItem:
     """
     Base class for any item that will be placed on a Board.
-    
-    :param type: A type you want to give your item. It can be any string. You can then use the type for sorting or grouping for example.
+
+    :param type: A type you want to give your item. It can be any string. You can then
+        use the type for sorting or grouping for example.
     :type type: str
     :param name: A name for this item. For identification purpose.
     :type name: str
-    :param pos: the position of this item. When the item is managed by the Board and Game engine this member hold the last updated position of the item. It is not updated if you manually move the item. It must be an array of 2 integers [row,column]
+    :param pos: the position of this item. When the item is managed by the Board and
+        Game engine this member hold the last updated position of the item. It is not
+        updated if you manually move the item. It must be an array of
+        2 integers [row,column]
     :type pos: array
-    :param model: The model to use to display this item on the Board. Be mindful of the space it will require. Default value is '*'.
+    :param model: The model to use to display this item on the Board. Be mindful of the
+        space it will require. Default value is '*'.
     :type model: str
     """
-    
-    def __init__(self,**kwargs):
-        self.name = 'Board item'
-        self.type = 'item'
-        self.pos = [None,None]
-        self.model = '*'
+
+    def __init__(self, **kwargs):
+        self.name = "Board item"
+        self.type = "item"
+        self.pos = [None, None]
+        self.model = "*"
         self.animation = None
         # Setting class parameters
-        for item in ['name','type','pos','model']:
+        for item in ["name", "type", "pos", "model"]:
             if item in kwargs:
-                setattr(self,item,kwargs[item])
+                setattr(self, item, kwargs[item])
 
     def __str__(self):
         return self.model
-    
+
     def __repr__(self):
         return self.model
-    
+
     def display(self):
         """
         Print the model WITHOUT carriage return.
         """
-        print(self.model,end='')
-    
+        print(self.model, end="")
+
     def debug_info(self):
         """
         Return a string with the list of the attributes and their current value.
@@ -45,16 +48,19 @@ class BoardItem():
         """
         string = "attrs: \n"
         for key in vars(self):
-            if type(getattr(self,key)) is list:
-                string += f"'{key}' = '"+ ''.join( str(e)+' ' for e in getattr(self,key)) + "'\n"
+            if type(getattr(self, key)) is list:
+                string += (
+                    f"'{key}' = '"
+                    + "".join(str(e) + " " for e in getattr(self, key))
+                    + "'\n"
+                )
             else:
-                string += f"'{key}' = '" + getattr(self,key) + "'\n"
+                string += f"'{key}' = '" + getattr(self, key) + "'\n"
         return string
 
-    
-    def store_position(self,x,y):
-        self.pos = [x,y]
-    
+    def store_position(self, x, y):
+        self.pos = [x, y]
+
     def can_move(self):
         """
         This is a virtual method that must be implemented in deriving classes.
@@ -75,7 +81,8 @@ class BoardItem():
         """
         This is a virtual method that must be implemented in deriving class.
         This method has to return True or False.
-        This represent the capacity for a BoardItem to be overlapped by another BoardItem.
+        This represent the capacity for a BoardItem to be overlapped by another
+        BoardItem.
         """
         raise NotImplementedError()
 
@@ -83,7 +90,8 @@ class BoardItem():
         """
         This is a virtual method that must be implemented in deriving class.
         This method has to return an integer.
-        This represent the size of the BoardItem. It is used for example to evaluate the space taken in the inventory.
+        This represent the size of the BoardItem. It is used for example to evaluate
+        the space taken in the inventory.
         """
         raise NotImplementedError()
 
@@ -92,9 +100,10 @@ class BoardItemVoid(BoardItem):
     """
     A class that represent a void cell.
     """
-    def __init__(self,**kwargs):
-        BoardItem.__init__(self,**kwargs)
-        self.name = 'void_cell'
+
+    def __init__(self, **kwargs):
+        BoardItem.__init__(self, **kwargs)
+        self.name = "void_cell"
 
     def pickable(self):
         """
@@ -103,12 +112,11 @@ class BoardItemVoid(BoardItem):
         :return: False
         """
         return False
-    
+
     def overlappable(self):
         """
-        A BoardItemVoid is obviously overlappable (so player and NPC can walk over). 
+        A BoardItemVoid is obviously overlappable (so player and NPC can walk over).
 
         :return: True
         """
         return True
-    
