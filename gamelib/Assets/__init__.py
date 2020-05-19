0185bb1bc0354382cea8a262c960c1f3097513ee
Arnaud Dupuis <arnaud.l.dupuis@gmail.com>
2020-05-19 02:42:35 -0700

    fix strings length.

--- gamelib/Assets/__init__.py
+++ gamelib/Assets/__init__.py
@@ -1,4 +1,4 @@
 """This is the Assets sub-module.
-Assets is a module that include a lot of ressources for game creation that are not core library features.
+Assets is a module that include a lot of ressources for game creation that are not core
+library features.
 """
-
