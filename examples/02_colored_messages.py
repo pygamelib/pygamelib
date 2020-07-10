a5a4bc38f248104267e4d054531e31cc1e0aebb1
Arnaud Dupuis <arnaud.l.dupuis@gmail.com>
2020-07-10 13:03:33 -0700

    First too examples transitioned to pygamelib

--- examples/02_colored_messages.py
+++ examples/02_colored_messages.py
@@ -1,21 +1,21 @@
 import examples_includes  # noqa: F401
 
-# First import the Utils module
-import gamelib.Utils as Utils
+# First import the Text object from the base module
+from pygamelib.base import Text
 
 # Then get creative!
 
 # you directly print colored messages:
-print(Utils.magenta_bright("This is a rather flashy magenta..."))
+print(Text.magenta_bright("This is a rather flashy magenta..."))
 
 # Each color function has 3 variations : regular, dim and bright
-print(Utils.yellow_dim("This is dim."))
-print(Utils.yellow("This is regular."))
-print(Utils.yellow_bright("This is bright."))
+print(Text.yellow_dim("This is dim."))
+print(Text.yellow("This is regular."))
+print(Text.yellow_bright("This is bright."))
 
 # Now, the color functions are just that: functions. So you can store the
 # results in a variable and use them:
-blue = Utils.blue_bright("blue")
-white = Utils.white_bright("white")
-red = Utils.red_bright("red")
+blue = Text.blue_bright("blue")
+white = Text.white_bright("white")
+red = Text.red_bright("red")
 print(f"France's flag is {blue} {white} {red}!")
