55322c14ecd1b0f2949369bc27ed026bed313187
Arnaud Dupuis <arnaud.l.dupuis@gmail.com>
2020-12-19 02:29:51 -0800

    Remove some useless code in buffer benchmark. Fix the strings length...

--- benchmark-screen-buffer.py
+++ benchmark-screen-buffer.py
@@ -1,4 +1,4 @@
-from pygamelib import engine, constants, board_items, base
+from pygamelib import engine, constants, board_items
 from pygamelib.gfx import core
 import time
 
@@ -12,7 +12,7 @@ def upd(g, k, dt):
 sc = core.SpriteCollection.load_json_file("tests/panda.spr")
 g = engine.Game(user_update=upd, mode=constants.MODE_TBT)
 g.player = board_items.Player(sprixel=core.Sprixel("@", None, core.Color(0, 255, 255)))
-g.add_board(1, engine.Board())
+g.load_board("hac-maps/test_editor.json", 1)
 g.change_level(1)
 # g.screen.place("+", int(g.screen.width / 2), int(g.screen.height / 2))
 
@@ -29,68 +29,7 @@ results.append(f"Benchmark runs at resolution: {g.screen.width}x{g.screen.height
 
 with g.terminal.cbreak(), g.terminal.hidden_cursor(), g.terminal.fullscreen():
 
-    #### BUFFER MULTIPROCESSING BENCHMARK
-    # for row in range(0, g.screen._display_buffer.shape[0]):
-    #     for col in range(0, g.screen._display_buffer.shape[1]):
-    #         g.screen.place(
-    #             core.Sprixel(" ", core.Color(row * 4, col, int((row + col) / 2))),
-    #             row,
-    #             col,
-    #         )
-    # count = 0
-    # start = time.time()
-    # while count < 50:
-    #     g.screen.update_mt()
-    #     count += 1
-    #     time.sleep(0.02)
-    # stop = time.time()
-    # results.append(
-    #     f"Benchmark (buffer MP): screen full redraw (50 times with 0.02  sec pause):"
-    #     f"\n\tTotal execution time: {stop-start} sec.\n\tTime per frame: "
-    #     f"{(stop-start-50*0.02)/50} sec.\n\tFPS: {1/((stop-start-50*0.02)/50)}"
-    # )
-    # for dt in test_times:
-    #     frames = 120
-    #     start = time.time()
-    #     frame_count = 0
-    #     while frame_count != frames and g.state != constants.STOPPED:
-    #         g.screen.place(f"Benchmark (buffer MP): {dt} {frame_count}/{frames}", 0, 0)
-    #         if k == "q":
-    #             g.state = constants.STOPPED
-
-    #         if g.player.column == g.screen.width - 1:
-    #             go = False
-    #         elif g.player.column == 0:
-    #             go = True
-    #         # g.screen.place(core.Sprixel(" "), g.player.row, g.player.column)
-    #         if isinstance(g.screen.buffer[g.player.row][g.player.column], core.Sprixel):
-    #             g.screen.buffer[g.player.row][g.player.column].model = " "
-    #             g.screen.buffer[g.player.row][g.player.column].fg_color = None
-    #         if go:
-    #             g.player.pos[1] += 1
-    #         else:
-    #             g.player.pos[1] -= 1
-    #         # g.screen.place(g.player, g.player.row, g.player.column)
-    #         if isinstance(g.screen.buffer[g.player.row][g.player.column], core.Sprixel):
-    #             g.screen.buffer[g.player.row][
-    #                 g.player.column
-    #             ].model = g.player.sprixel.model
-    #             g.screen.buffer[g.player.row][
-    #                 g.player.column
-    #             ].fg_color = g.player.sprixel.fg_color
-    #         g.screen._is_dirty = True
-    #         g.screen.update()
-    #         k = g.terminal.inkey(timeout=dt)
-    #         frame_count += 1
-    #     stop = time.time()
-    #     results.append(
-    #         f"Benchmark (buffer MP) for :\n\tdt={dt}\n\tframes rendered={frame_count} in "
-    #         f"{stop - start} sec. or {(stop-start)/frame_count} sec. per frame\n\t"
-    #         f"Actual rendering time per frame: {(stop-start)/frame_count - dt} sec.\n\t"
-    #         f"FPS: {1/((stop-start)/frame_count - dt)}"
-    #     )
-
-    #### BUFFER BENCHMARK
+    # **BUFFER BENCHMARK**
     for row in range(0, g.screen._display_buffer.shape[0]):
         for col in range(0, g.screen._display_buffer.shape[1]):
             g.screen.place(
@@ -108,21 +47,15 @@ with g.terminal.cbreak(), g.terminal.hidden_cursor(), g.terminal.fullscreen():
     results.append(
         f"Benchmark (buffer): screen full redraw (50 times with 0.02  sec pause):"
         f"\n\tTotal execution time: {stop-start} sec.\n\tTime per frame: "
-        f"{(stop-start-50*0.02)/50} sec.\n\tFPS: {1/((stop-start-50*0.02)/50)}"
+        f"{((stop-start-50*0.02)/50)*1000} msec.\n\tFPS: {1/((stop-start-50*0.02)/50)}"
     )
-    g.screen.place(sc["panda"], 20, 20)
+    pr = pc = 20
+    g.screen.place(sc["panda"], pr, pc)
     for dt in test_times:
         frames = 120
         start = time.time()
         frame_count = 0
         while frame_count != frames and g.state != constants.STOPPED:
-            # g.screen.place(
-            #     core.Sprite.from_text(
-            #         base.Text(f"Benchmark (buffer): {dt} {frame_count}/{frames}")
-            #     ),
-            #     0,
-            #     0,
-            # )
             g.screen.place(
                 f"Benchmark (buffer): {dt} {frame_count}/{frames}", 0, 0,
             )
@@ -155,6 +88,16 @@ with g.terminal.cbreak(), g.terminal.hidden_cursor(), g.terminal.fullscreen():
                 int((g.player.row + g.player.column) / 2),
             )
             g.screen.place(g.player, g.player.row, g.player.column)
+            # Test moving a Sprite
+            g.screen.place(
+                core.Sprixel(" ", core.Color(pr * 4, pc, int((pr + pc) / 2),),), pr, pc
+            )
+            if (pr + sc["panda"].height) < (g.screen.height - 1):
+                pr += 1
+            if (pc + sc["panda"].width) < (g.screen.width - 1):
+                pc += 1
+            g.screen.place(sc["panda"], pr, pc)
+            # Update screen (this will trigger Screen.render())
             g.screen.update()
             k = g.terminal.inkey(timeout=dt)
             frame_count += 1
@@ -162,11 +105,11 @@ with g.terminal.cbreak(), g.terminal.hidden_cursor(), g.terminal.fullscreen():
         results.append(
             f"Benchmark (buffer) for :\n\tdt={dt}\n\tframes rendered={frame_count} in "
             f"{stop - start} sec. or {(stop-start)/frame_count} sec. per frame\n\t"
-            f"Actual rendering time per frame: {(stop-start)/frame_count - dt} sec.\n\t"
-            f"FPS: {1/((stop-start)/frame_count - dt)}"
+            f"Actual rendering time per frame: {((stop-start)/frame_count - dt)*1000} "
+            f"msec.\n\tFPS: {1/((stop-start)/frame_count - dt)}"
         )
 
-    ### DISPLAY/BLESSED BENCHMARK
+    # **DISPLAY/BLESSED BENCHMARK**
     g.clear_screen()
     # print(g.terminal.home, end="")
     count = 0
@@ -184,7 +127,9 @@ with g.terminal.cbreak(), g.terminal.hidden_cursor(), g.terminal.fullscreen():
         time.sleep(0.02)
     stop = time.time()
     results.append(
-        f"Benchmark (blessed): screen full redraw (50 times with 0.02  sec pause):\n\tTotal execution time: {stop-start} sec.\n\tTime per frame: {(stop-start-50*0.02)/50} sec.\n\tFPS: {1/((stop-start-50*0.02)/50)}"
+        f"Benchmark (blessed): screen full redraw (50 times with 0.02  sec pause):\n\t"
+        f"Total execution time: {stop-start} sec.\n\tTime per frame: "
+        f"{((stop-start-50*0.02)/50)*1000} msec.\n\tFPS: {1/((stop-start-50*0.02)/50)}"
     )
 
     for dt in test_times:
@@ -232,7 +177,10 @@ with g.terminal.cbreak(), g.terminal.hidden_cursor(), g.terminal.fullscreen():
             frame_count += 1
         stop = time.time()
         results.append(
-            f"Benchmark (blessed) for :\n\tdt={dt}\n\tframes rendered={frame_count} in {stop - start} sec. or {(stop-start)/frame_count} sec. per frame\n\tActual rendering time per frame: {(stop-start)/frame_count - dt} sec.\n\tFPS: {1/((stop-start)/frame_count - dt)}"
+            f"Benchmark (blessed) for :\n\tdt={dt}\n\tframes rendered={frame_count} in "
+            f"{stop - start} sec. or {(stop-start)/frame_count} sec. per frame\n\t"
+            f"Actual rendering time per frame: {((stop-start)/frame_count - dt)*1000} "
+            f"msec.\n\tFPS: {1/((stop-start)/frame_count - dt)}"
         )
 
 print("\n=========== Benchmark results ===========")
