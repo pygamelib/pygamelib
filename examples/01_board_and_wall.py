f78f050bee98f427ab322f2e347c279a766727e7
Arnaud Dupuis <arnaud.l.dupuis@gmail.com>
2022-04-20 19:42:07 -0700

    Update examples to make them compliant with v1.3.0

--- examples/01_board_and_wall.py
+++ examples/01_board_and_wall.py
@@ -6,6 +6,9 @@ import pygamelib.engine as engine
 # Then some graphics to display nice colored squares.
 import pygamelib.assets.graphics as graphics
 
+# And gfx.core to do the same but with sprixels.
+from pygamelib.gfx import core
+
 # And finally board_items as it hold everything we can put on a board.
 import pygamelib.board_items as board_items
 
@@ -19,15 +22,15 @@ myboard = engine.Board(
     ui_border_right=graphics.WHITE_SQUARE,
     ui_border_top=graphics.WHITE_SQUARE,
     ui_border_bottom=graphics.WHITE_SQUARE,
-    ui_board_void_cell=graphics.BLACK_SQUARE,
+    ui_board_void_cell_sprixel=core.Sprixel.black_square(),
     player_starting_position=[10, 20],
 )
 
-# Now let's make a couple of walls using colored squares as models
-green_wall = board_items.Wall(model=graphics.GREEN_SQUARE)
-blue_wall = board_items.Wall(model=graphics.BLUE_SQUARE)
-cyan_wall = board_items.Wall(model=graphics.CYAN_SQUARE)
-magenta_wall = board_items.Wall(model=graphics.MAGENTA_SQUARE)
+# Now let's make a couple of walls using colored squares as sprixels
+green_wall = board_items.Wall(sprixel=core.Sprixel.green_square())
+blue_wall = board_items.Wall(sprixel=core.Sprixel.blue_square())
+cyan_wall = board_items.Wall(sprixel=core.Sprixel.cyan_square())
+magenta_wall = board_items.Wall(sprixel=core.Sprixel.magenta_square())
 
 # Now let's place these walls on the board
 for k in range(1, 6, 1):
@@ -38,9 +41,13 @@ for k in range(2, 6, 1):
     myboard.place_item(blue_wall, k, 1)
     myboard.place_item(cyan_wall, 5, k)
 
-# And just for the fun of it, let's place a yellow and cyan wall
+# And just for the fun of it, let's place a orange-ish colored wall
 # in the middle of that "structure"
-myboard.place_item(board_items.Wall(model=graphics.YELLOW_CYAN_SQUARE), 3, 3)
+myboard.place_item(
+    board_items.Wall(sprixel=core.Sprixel("  ", core.Color(255, 123, 28))),
+    3,
+    3,
+)
 
 # Finally let's display the board
 myboard.display()
