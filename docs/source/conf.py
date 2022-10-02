e592d7dd44a93034020fcaf07b56498554e4b72c
Arnaud Dupuis <arnaud.l.dupuis@gmail.com>
2022-10-02 00:24:08 -0700

    Add docs templates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,7 +52,8 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["ntemplates"]
+# templates_path = ["ntemplates"]
+templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -68,12 +69,13 @@ master_doc = "index"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patterns = []
+exclude_patterns = ["_build", "_templates"]
+# exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = "sphinx"
